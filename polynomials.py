8bebf1ea6dff8e7e88526851a3884eeaac5243f7
David Ben-Gurion <rnithin@berkeley.edu>
2018-04-09 03:19:05 -0700

    removed unnecessary code

--- polynomials.py
+++ polynomials.py
@@ -100,21 +100,3 @@ def make_comparator(var : str):
                 return compare(x_val, y_val)
     return K
 
-def cmp_to_key(make_comparator, var : str):
-    'Convert a cmp= function into a key= function'
-    class K(object):
-        def __init__(self, obj, *args):
-            self.obj = obj
-        def __lt__(self, other):
-            return make_comparator(var)(self.obj, other.obj) < 0
-        def __gt__(self, other):
-            return make_comparator(var)(self.obj, other.obj) > 0
-        def __eq__(self, other):
-            return make_comparator(var)(self.obj, other.obj) == 0
-        def __le__(self, other):
-            return make_comparator(var)(self.obj, other.obj) <= 0
-        def __ge__(self, other):
-            return make_comparator(var)(self.obj, other.obj) >= 0
-        def __ne__(self, other):
-            return make_comparator(var)(self.obj, other.obj) != 0
-    return K
